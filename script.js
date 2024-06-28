81db7207ae2ceb356c91e38b0eb11bc3d97f47e2
Farhan Danish <codesbyfarhan@gmail.com>
2024-06-28 23:24:04 +0530

    Implementing The Calculator Function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let nob = document.getElementById('NOB');
 let Nob_values = document.getElementsByName("band-value");
 let value_boxes = document.querySelectorAll('.value-box');
+let number_of_bands = 3;
 class pHandler{
     constructor(){
         this.view_close_options();
-        this.view_color_grid();
         this.getBands()
+        this.view_color_grid();
     }
 
     view_close_options(){
@@ -30,7 +31,8 @@ class pHandler{
     getBands(){
         Nob_values.forEach(element => {
             element.addEventListener("click",(e)=>{
-               this.display_Band_Box(e.target.value)
+               number_of_bands = e.target.value;
+               this.display_Band_Box(number_of_bands);
             })
         });
     }
@@ -58,9 +60,10 @@ class pHandler{
     }
 
     view_color_grid(){
+        let filled_boxes = 0;
         let color_grid = document.createElement('div');
         color_grid.className="color-grid";
-        color_grid.innerHTML='<span id="black"></span><span id="brown"></span><span id="red"></span><span id="orange"></span><span id="yellow"></span><span id="green"></span><span id="blue"></span id="violet"><span id="grey"></span><span id="white"></span><span id="gold"></span><span id="silver"></span>';
+        color_grid.innerHTML='<span id="black"></span><span id="brown"></span><span id="red"></span><span id="orange"></span><span id="yellow"></span><span id="green"></span><span id="blue"></span><span id="violet"></span><span id="grey"></span><span id="white"></span><span id="gold"></span><span id="silver"></span>';
         let colors = color_grid.querySelectorAll('span');
         for(let box of value_boxes){
             box.firstElementChild.addEventListener('click',(e)=>{
@@ -69,9 +72,49 @@ class pHandler{
         }
         for (let color of colors) {
             color.addEventListener('click',e => {
+                let shown_boxes = document.querySelectorAll(".show");
                 e.currentTarget.parentElement.parentElement.style.background=`${e.currentTarget.id}`;
-                e.currentTarget.parentElement.remove()
+                e.currentTarget.parentElement.remove();
+                filled_boxes++;
+                if(filled_boxes==shown_boxes.length){
+                    filled_boxes--;
+                    this.Calculator(number_of_bands);
+                }
             })
+        }
+    }
+    Calculator(Bands){
+        let d1 = document.getElementById("Digit1").style.background;
+        let d2 = document.getElementById("Digit2").style.background;
+        let d3 = document.getElementById("Digit3").style.background;
+        let m = document.getElementById("Multiplier").style.background;
+        let tol = document.getElementById("Tolerance").style.background;
+        let temp = document.getElementById("Temparture").style.background;
+        let Resistance = 0;
+        let Tolerance = 0;
+        let Temparture = 0;
+        let Values = {
+            "black":[0,0,0,1,'',250],
+            "brown":[1,1,1,10,10,100],
+            "red":[2,2,2,100,2,50],
+            "orange":[3,3,3,1000,'',15],
+            "yellow":[4,4,4,10000,'',25],
+            "green":[5,5,5,100000,0.5,20],
+            "blue":[6,6,6,1000000,0.25,10],
+            "violet":[7,7,7,'',0.1,5],
+            "Grey":[8,8,8,'','',1],
+            "white":[9,9,9,'','',''],
+            "gold":['','','',0.1,5,''],
+            "silver":['','','',0.01,10,'']
+        }
+        switch (Bands) {
+            case 3:
+                Resistance=(((Values[d1][0]*10)+Values[d2][1])*Values[m][3]);
+                console.log(Resistance);
+                break;
+        
+            default:
+                break;
         }
     }
 }
